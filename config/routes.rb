3de5d46615f609511a447faad21349b415640196
LHY927 <liuhaiyue0927@gmail.com>
2024-05-14 21:29:31 +0200

    Feature User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+  get 'users/create'
   namespace :admin do
     get 'categories/index'
     get 'categories/new'
@@ -27,6 +29,15 @@ Rails.application.routes.draw do
     resources :products
     resources :categories, only: [:index, :new, :create]
   end
+
+  get 'register', to: 'users#new'
+  post 'register', to: 'users#create'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
